54fab951918456c9aac476e14813329bb232a600
REMCodes <rowan.mcalpin@gmail.com>
2024-10-22 12:55:08 -0700

    Migrated color sensor code to new repo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleFeedbackLights.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleFeedbackLights.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Light;
 
 public class SampleFeedbackLights {
+    private static Light SampleLights;
     public static void initLights(OpMode opmode) {
 
     }
