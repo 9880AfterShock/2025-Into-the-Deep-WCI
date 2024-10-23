70bd1b292659b5d2eb1d23cff5487ce21cd77777
DaBiggestBozo <146139125+DaBiggestBozo@users.noreply.github.com>
2024-10-23 16:19:17 -0700

    made frantic park auto for scrimmage lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TempAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TempAuto.java
@@ -1,34 +1,53 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name="Skrimage Auto")
 public class TempAuto extends LinearOpMode {
-    private DcMotor leftDrive   = null;
-    private DcMotor rightDrive  = null;
+    private DcMotor frontLeftDrive   = null;
+    private DcMotor frontRightDrive  = null;
+    private DcMotor backLeftDrive   = null;
+    private DcMotor backRightDrive  = null;
     static final double FORWARD_SPEED = 0.6;
+    static final double STOP_SPEED = 0.0;
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
 
         frontLeftDrive  = hardwareMap.get(DcMotor.class, "leftFront");
         frontRightDrive = hardwareMap.get(DcMotor.class, "rightFront");
-        backLeftDrive  = hardwareMap.get(DcMotor.class, "rightRear");
+        backLeftDrive  = hardwareMap.get(DcMotor.class, "leftRear");
         backRightDrive = hardwareMap.get(DcMotor.class, "rightRear");
 
-        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
-        // JUST STOP THE ROBOT CONTROLLER WHEN ITS DONE
-      
-        leftDrive.setPower(FORWARD_SPEED);
-        rightDrive.setPower(FORWARD_SPEED);
+        runtime.reset();
+
+        frontLeftDrive.setPower(FORWARD_SPEED);
+        frontRightDrive.setPower(FORWARD_SPEED);
+        backLeftDrive.setPower(FORWARD_SPEED);
+        backRightDrive.setPower(FORWARD_SPEED);
+
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            sleep(10);
+        }
+
+        frontLeftDrive.setPower(STOP_SPEED);
+        frontRightDrive.setPower(STOP_SPEED);
+        backLeftDrive.setPower(STOP_SPEED);
+        backRightDrive.setPower(STOP_SPEED);
+
+        while (opModeIsActive()) {
+            sleep(1);
+        }
     }
 }
