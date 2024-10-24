42246eb2d4393933ea55cabcdc5061686972890a
Hillel Coates <hillel.g.coates@gmail.com>
2024-10-24 13:55:46 -0700

    Downgrade and unify Android and Kotlin versions across the project
    *Downgrading and standardizing the project versions to prevent compatibility issues*
    
    - Changed `compileSdk` and `compileSdkVersion` to `29` in `FtcRobotController`, `build.common.gradle` to standardize the Android API version across the project
    - Downgraded `kotlin_version` to `1.9.0` in `build.gradle` to match the Android API version and prevent version conflicts
    - Changed `javaVersion` to `1.8` in `MeepMeep` to align with the `kotlin_version` and prevent potential Java version issues
    - Moved `maven { url = 'https://maven.brott.dev/' }` repository to `build.dependencies.gradle` to centralize the repository configuration
    - Unified `kotlin-android` and `kotlinOptions` configurations in `TeamCode` to match the project standards
    - Changed `org.jetbrains.kotlin.jvm` version to match the project's `kotlin_version` in `settings.gradle`
    - Updated `android.aapt2FromMavenOverride` in `gradle.properties` to a newer version for compatibility reasons
    - Removed redundant repository and plugin configurations across various build files to tidy up the project setup

--- MeepMeep/build.gradle.kts
+++ MeepMeep/build.gradle.kts
@@ -3,16 +3,13 @@ plugins {
     id("kotlin")
 }
 repositories {
-    mavenCentral()
-    mavenLocal()
-    google()
     maven {
         url = uri("https://maven.brott.dev/")
     }
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_18 // both use 17, 18 just works? unsupported officall and 17 works too
-    targetCompatibility = JavaVersion.VERSION_18 // both use 17, 18 just works? unsupported officall and 17 works too
+    sourceCompatibility = JavaVersion.VERSION_1_8 // both use 17, 18 just works? unsupported officall and 17 works too
+    targetCompatibility = JavaVersion.VERSION_1_8 // both use 17, 18 just works? unsupported officall and 17 works too
 }
 dependencies {
     //implementation(project(":FtcRobotController")) //both commented out
