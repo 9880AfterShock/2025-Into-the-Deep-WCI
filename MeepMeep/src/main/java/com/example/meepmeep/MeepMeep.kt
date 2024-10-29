2129996aff8f96b385c5de4e03b39dcee266d9b3
cadenthecreator <cadendanielream@outlook.com>
2024-10-29 15:05:49 -0700

    MeepMeep auto modeling

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeep.kt
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeep.kt
@@ -2,6 +2,7 @@ package com.example.meepmeep
 // 1.13.1 ktx version
 
 import com.acmerobotics.roadrunner.Pose2d
+import com.acmerobotics.roadrunner.Vector2d
 import com.noahbres.meepmeep.MeepMeep
 import com.noahbres.meepmeep.MeepMeep.Background
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder
@@ -14,23 +15,23 @@ object MeepMeep {
             DefaultBotBuilder(meepMeep) // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60.0, 60.0, Math.toRadians(180.0), Math.toRadians(0.0), 17.0)
                 .build()
-
         myBot.runAction(
-            myBot.drive.actionBuilder((Pose2d(20.0, 61.0, 0.0)))
-                //run claw flip
-                // run arm up
-                .splineToSplineHeading(Pose2d(0.0, 34.0, Math.toRadians(45.0)), 0.125)
-                .waitSeconds(0.5)
-                // run arm extend
-                .waitSeconds(1.5)
-                // run claw release
-                .waitSeconds(1.0)
-                // run claw grab
-                .waitSeconds(0.5)
-                // run arm unextend
-                .waitSeconds(2.0)
-                // leave arm up
-                .splineToSplineHeading(Pose2d(60.0, -60.0, Math.toRadians(90.0)), Math.toRadians(-50.0))
+            myBot.drive.actionBuilder((Pose2d(0.4, 59.2, 0.0)))
+                .splineToLinearHeading(Pose2d(0.0,34.8,Math.toRadians(180.0)),0.0)
+                .waitSeconds(4.0)
+                .strafeToLinearHeading(Vector2d(-34.8,40.4),Math.toRadians(90.0))
+                .strafeTo(Vector2d(-34.8,12.1))
+                .strafeToLinearHeading(Vector2d(-43.9,12.1),Math.toRadians(90.0))
+                .strafeToLinearHeading(Vector2d(-43.9, 50.0), Math.toRadians(90.0))
+                .strafeToLinearHeading(Vector2d(-43.9,12.1),Math.toRadians(90.0))
+                .strafeToLinearHeading(Vector2d(-52.2,12.1),Math.toRadians(90.0))
+                .strafeToLinearHeading(Vector2d(-52.2, 50.0), Math.toRadians(90.0))
+                .strafeToLinearHeading(Vector2d(-52.2,60.2),0.0)
+                .waitSeconds(4.0)
+                .strafeToLinearHeading(Vector2d(-33.9,42.1),Math.toRadians(180.0))
+                .splineToLinearHeading(Pose2d(0.0,34.8,Math.toRadians(180.0)),5.0)
+                .waitSeconds(4.0)
+                .strafeToLinearHeading(Vector2d(-60.0,60.0,),Math.toRadians(180.0))
                 .build()
         )
 
