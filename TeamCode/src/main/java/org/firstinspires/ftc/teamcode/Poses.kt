ec9a9e87be0642f042190f5668bce39675b55949
Morriswell <pioltoscarrothwell@gmail.com>
2024-11-05 14:59:45 -0800

    ?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Poses.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Poses.kt
@@ -5,11 +5,11 @@ import com.acmerobotics.roadrunner.Pose2d
 
 // Poses
     // blue side
-    var startPoseBlue = Pose2d(61.0, 20.0, 0.0)
-    var clipPoseBlue = Pose2d(34.0, 0.0, Math.toRadians(45.0))
-    var parkPoseBlue = Pose2d(60.0, -60.0, Math.PI * 1.5)
+    var startPoseBlue = Pose2d(-14.0, 61.0, 0.0)
+    var clipPoseBlue = Pose2d(0.0, 31.0, Math.toRadians(45.0))
+    var parkPoseBlue = Pose2d(-60.0, 60.0, Math.PI * 1.5)
     // red side
-    var startPoseRed = Pose2d(15.0, -61.0, Math.toRadians(180.0))
-    var clipPoseRed = Pose2d(0.0, -34.0, Math.toRadians(180.0))
+    var startPoseRed = Pose2d(14.0, -61.0, Math.toRadians(180.0))
+    var clipPoseRed = Pose2d(0.0, -31.0, Math.toRadians(180.0))
     var parkPoseRed = Pose2d(60.0, -60.0, Math.toRadians(90.0))
 // Paths
