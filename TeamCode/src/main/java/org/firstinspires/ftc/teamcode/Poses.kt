f877bba81c9892f551fb8dfec4528505f2dccbd0
cadenthecreator <cadendanielream@outlook.com>
2024-11-14 13:59:38 -0800

    auto op mode blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Poses.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Poses.kt
@@ -5,14 +5,15 @@ import com.acmerobotics.roadrunner.Vector2d
 
 
 // Poses
-    // blue side
-    var startPoseBlue = Pose2d(-14.0, 61.0, 0.0)
-    var clipPoseBlue = Pose2d(0.0, 30.0, Math.toRadians(45.0))
-    var parkPoseBlue = Pose2d(-60.0, 60.0, Math.PI * 1.5)
-    // red side
-    var startPoseRed = Pose2d(14.0, -61.0, Math.toRadians(180.0))
-    var clipPoseRed = Pose2d(7.0, -32.5, Math.toRadians(180.0))
-    //var backVecRed = Vector2d(0.0, -30.0)
-    var backPoseRed = Pose2d(0.0, -40.0, Math.toRadians(180.0))
-    var parkPoseRed = Pose2d(60.0, -60.0, Math.toRadians(90.0))
+// blue side
+var startPoseBlue = Pose2d(-14.0, 61.0, 0.0)
+var clipPoseBlue = Pose2d(0.0, 32.5, 0.0)
+var parkPoseBlue = Pose2d(-60.0, 60.0, Math.toRadians(-90.0))
+var backPoseBlue = Pose2d(0.0, 40.0, 0.0)
+// red side
+var startPoseRed = Pose2d(14.0, -61.0, Math.toRadians(180.0))
+var clipPoseRed = Pose2d(7.0, -32.5, Math.toRadians(180.0))
+//var backVecRed = Vector2d(0.0, -30.0)
+var backPoseRed = Pose2d(0.0, -40.0, Math.toRadians(180.0))
+var parkPoseRed = Pose2d(60.0, -60.0, Math.toRadians(90.0))
 // Paths
