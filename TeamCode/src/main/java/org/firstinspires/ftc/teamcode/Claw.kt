453a6c59170a63d2fdf1b23f4f1d9393da55057f
DaBiggestBozo <146139125+DaBiggestBozo@users.noreply.github.com>
2024-11-24 08:16:05 -0800

    added claw rr actions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.kt
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode
 
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.acmerobotics.roadrunner.Action
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.hardware.Servo
+import org.firstinspires.ftc.teamcode.Raiser.motor
+import org.firstinspires.ftc.teamcode.Raiser.upPos
 
 
 object Claw {
@@ -51,5 +55,17 @@ object Claw {
         }
         clawButtonPreviouslyPressed = clawButtonCurrentlyPressed
 
+    }
+    class autoClawOpen: Action {
+        override fun run(p: TelemetryPacket): Boolean {
+            open()
+            return false
+        }
+    }
+    class autoClawClose: Action {
+        override fun run(p: TelemetryPacket): Boolean {
+            close()
+            return false
+        }
     }
 }
