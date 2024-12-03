8459c00804a0e1b5bf7fab0b4260e20aa65edec5
Morriswell <pioltoscarrothwell@gmail.com>
2024-12-03 10:03:24 -0800

    going to merge

--- MeepMeep/src/main/java/com/example/meepmeep/Poses.kt
+++ MeepMeep/src/main/java/com/example/meepmeep/Poses.kt
@@ -5,15 +5,32 @@ import com.acmerobotics.roadrunner.Vector2d
 
 
 // Poses
-    // blue side
-    var startPoseBlue = Pose2d(-14.0, 61.0, 0.0)
-    var clipPoseBlue = Pose2d(0.0, 32.5, 0.0)
-    var parkPoseBlue = Pose2d(-60.0, 60.0, Math.toRadians(-90.0))
-    var backPoseBlue = Pose2d(0.0, 40.0, 0.0)
-    // red side
-    var startPoseRed = Pose2d(14.0, -61.0, Math.toRadians(180.0))
-    var clipPoseRed = Pose2d(7.0, -32.5, Math.toRadians(180.0))
-    //var backVecRed = Vector2d(0.0, -30.0)
-    var backPoseRed = Pose2d(0.0, -40.0, Math.toRadians(180.0))
-    var parkPoseRed = Pose2d(60.0, -60.0, Math.toRadians(90.0))
+// blue side
+var startPoseBlueBucket = Pose2d(32.8, 59.2, 0.0)
+var neutralPoseBlue = Pose2d(35.0, 26.0,0.0)
+var bucketPoseBlue = Pose2d(47.1, 46.5, Math.toRadians(45.0))
+var bucketParkPoseBlue = Pose2d(26.0, 0.0, Math.toRadians(180.0))
+var startPoseBlue = Pose2d(-14.0, 61.0, 0.0) // use for big
+var clipPoseBlue = Pose2d(-7.0, 33.25, 0.0) // use for big
+var backPoseBlue = Pose2d(0.0, 40.0, 0.0)
+var parkPoseBlue = Pose2d(-57.0, 57.0, Math.toRadians(-90.0))
+
+// red side
+var startPoseRed = Pose2d(14.0, -61.0, Math.toRadians(180.0))
+var clipPoseRed = Pose2d(7.0, -33.25, Math.toRadians(180.0))
+var backPoseRed = Pose2d(0.0, -40.0, Math.toRadians(180.0))
+var parkPoseRed = Pose2d(57.0, -57.0, Math.toRadians(90.0))
+// big auto
+var pushPrepPoseBig = Pose2d(-34.8,12.1, Math.PI/2)
+var pushPrepPoseRightBig = Vector2d(-43.9,12.1) //good
+var pushPoseRightBig = Vector2d(-43.9, 50.0) //good
+var pushPrepPoseMidBig = Vector2d(-52.2,12.1) // good
+var pushPoseMidBig = Vector2d(-52.2, 50.0) //good
+var specStartPickupPoseBig = Pose2d(-35.2, 58.0, Math.PI) // good original x is -36.2
+var specEndPickupPoseBig = Pose2d(-42.2,57.95, Math.PI) // test for moving
+var specStartPickupPoseLastBig = Pose2d(-36.2, 59.2, Math.PI)
+var specEndPickupPoseLastBig = Pose2d(-41.2, 60.2, Math.PI)
+var clipPoseBlueTheSecond = Pose2d(0.0, 32.8, 0.0)
+var clipPoseBlueTheThird = Pose2d(5.0, 33.85, 0.0)
+var parkPoseBlueBig = Pose2d(-53.0, 57.0, Math.toRadians(0.0)) // use for big
 // Paths
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -91,13 +91,16 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 12.5;// inches per second per inches of error for all 3 of the thingies, 12.5
-        public double lateralGain = 12.5;// 12,5
-        public double headingGain = 4.0; // shared with turn, was 15., 4.0
+        // old values might be fine for the gains. The vel gains are the primary issues.
+        // decreasing the vel gains are the most important. just like the D of PID. lower than 1.4.
+        // probly 1 or greater for vel gains.
+        public double axialGain = 1.0;// inches per second per inches of error for all 3 of the thingies, 12.5
+        public double lateralGain = 1.0;// 12,5
+        public double headingGain = 8.0; // shared with turn, was 15., 4.0
 
-        public double axialVelGain = 2.0;// inches per idk? maybe??? idk. :0 :0 :0
-        public double lateralVelGain = 1.4;
-        public double headingVelGain = 1.6; // shared with turn
+        public double axialVelGain = 2.0;// inches per idk? maybe??? idk. :0 :0 :0 //2.0
+        public double lateralVelGain = 1.4; // 1.4
+        public double headingVelGain = 1.0; // shared with turn // 1.6
     }
 
     public static Params PARAMS = new Params();
