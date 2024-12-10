2b02893f0010fef33867308c5dd40ff016cdd01a
DaBiggestBozo <146139125+DaBiggestBozo@users.noreply.github.com>
2024-12-10 15:21:12 -0800

    added right control for beard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Beard.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Beard.kt
@@ -39,7 +39,7 @@ object Beard {
     }
     fun updateBeard() {
         opmode.telemetry.addData("Beard Position", state)
-        beardButtonCurrentlyPressed = (opmode.gamepad1.right_trigger.toDouble() > 0.5) //change this to change the button
+        beardButtonCurrentlyPressed = (opmode.gamepad2.right_trigger.toDouble() > 0.5) //change this to change the button
 
         if (beardButtonCurrentlyPressed && !beardButtonPreviouslyPressed) {
             swap()
