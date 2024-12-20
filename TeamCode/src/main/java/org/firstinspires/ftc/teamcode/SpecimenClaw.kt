847a6a16b218eea16ee9fe107168ca1c14e3d950
Morriswell <pioltoscarrothwell@gmail.com>
2024-12-20 11:52:55 -0800

    auto speedadb disconnectadb disconnect

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpecimenClaw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpecimenClaw.kt
@@ -80,7 +80,7 @@ object SpecimenClaw {
     }
     class autoDelaySpecClawClose: Action {
         override fun run(p: TelemetryPacket): Boolean {
-            sleep(100)
+            sleep(350)
             close()
             p.put("claw done", 1.0)
             return false
