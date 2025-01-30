22236a07626c4c665ddd68f9b5a194b8d516ab9a
Morriswell <pioltoscarrothwell@gmail.com>
2025-01-30 14:28:17 -0800

    final changes for thursday b4 saturday comp when it comes to the hecken gosh darn thingey, uhh, the auto clip side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Poses.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Poses.kt
@@ -29,7 +29,7 @@ var pushPoseRightBigFast = Pose2d(-43.9, 46.0,Math.toRadians(90.0)) //good
 var pushPrepPoseMidBigFast = Pose2d(-53.2,12.1,Math.toRadians(90.0)) // good
 var pushPoseMidBigFast = Pose2d(-52.2, 46.0,Math.toRadians(90.0)) //good
 //
-var pushPrepPoseRightBigFastAsstronomical = Pose2d(-35.8,12.1,Math.toRadians(90.0)) //good // was -35
+var pushPrepPoseRightBigFastAsstronomical = Pose2d(-37.5,12.1,Math.toRadians(90.0)) //good // was -35.8
 var pushPoseRightBigFastAsstronomical = Pose2d(-43.9, 46.0,Math.toRadians(90.0)) //good
 var pushPrepPoseMidBigFastAsstronomical = Pose2d(-51.2,12.1,Math.toRadians(90.0)) // good
 var pushPoseMidBigFastAsstronomical = Pose2d(-50.0, 46.0,Math.toRadians(90.0)) //good
@@ -48,9 +48,9 @@ var specStartPickupPoseLastBig = Pose2d(-33.2, 59.25, Math.PI)// in need of pose
 var specEndPickupPoseLastBig = Pose2d(-36.7, 58.25, Math.PI)// in need of poses
 //
 var specStartPickupPoseBigAsstronomical = Pose2d(-37.7, 57.15, Math.PI) // good original x is -36.2
-var specEndPickupPoseBigAsstronomical = Pose2d(-39.2,56.5, Math.PI) // test for moving
+var specEndPickupPoseBigAsstronomical = Pose2d(-40.2,56.5, Math.PI) // test for moving
 var specStartPickupPoseSecondBigAsstronomical = Pose2d(-34.85, 58.155, Math.PI)//505
-var specEndPickupPoseSecondBigAsstronomical = Pose2d(-40.7, 58.3, Math.PI)//-42.2, 60.1, math.pi
+var specEndPickupPoseSecondBigAsstronomical = Pose2d(-39.8, 58.3, Math.PI)//-42.2, 60.1, math.pi
 var specStartPickupPoseLastBigAsstronomical = Pose2d(-33.2, 59.25, Math.PI)// in need of poses
 var specEndPickupPoseLastBigAsstronomical = Pose2d(-36.7, 58.25, Math.PI)// in need of poses
 //
@@ -61,7 +61,7 @@ var clipPoseBlueTheFourth = Pose2d(3.0, 31.0, 0.0)// in need of Poses
 var clipPoseBlueAsstronomical = Pose2d(-11.0, 32.0 , 0.0) // use for big
 var clipPoseBlueTheSecondAsstronomical = Pose2d(-7.0, 29.00, 0.0)//0.0, 31.750, 0.0// 31.250 // 31.000
 var clipPoseBlueTheThirdAsstronomical = Pose2d(-2.0, 30.83, 0.0)// 29.6800
-var clipPoseBlueTheFourthAsstronomical = Pose2d(3.0, 31.0, 0.0)// in need of Poses
+var clipPoseBlueTheFourthAsstronomical = Pose2d(3.0, 31.55, 0.0)// in need of Poses
 var clipPoseBlueTheFifthAsstronomical = Pose2d(3.0, 31.0, 0.0)// in need of Poses
 //
 var parkPoseBlueBig = Pose2d(-50.0, 57.0, Math.toRadians(0.0)) // use for big
