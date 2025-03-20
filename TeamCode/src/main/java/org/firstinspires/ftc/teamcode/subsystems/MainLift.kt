d4f1d61f8e5d3cb79aeb9b6c8137135a401b4582
DaBiggestBozo <146139125+DaBiggestBozo@users.noreply.github.com>
2025-03-20 14:11:02 -0700

    made manual extend faster for deeper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MainLift.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MainLift.kt
@@ -15,7 +15,7 @@ object MainLift { //Prefix for commands
     var pos = 0.0 //starting Position
     var currentSpeed = 0.0 //Starting speed, WHY ARE YOU MAKING A FALLING LIFT???
     @JvmField
-    var speed = 0.02 //update speed
+    var speed = 0.03 //update speed
     val encoderTicks = 751.8 //calculate your own ratio///// old us 537.7
     @JvmField
     var minPos = 0.0 //folded all the way in
